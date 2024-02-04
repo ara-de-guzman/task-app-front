c4bb71264ebd0e314c33424dc76d9941e2620326
Ara De Guzman <aradeguzman@aradesign.tech>
2024-02-03 16:02:39 -0800

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8000/api/users/";
+const API_URL = "https://taskbackend-yqw0nfw1.b4a.run/api/users/";
 
 
 const register = async (userData) => {
